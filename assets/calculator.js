e861136f779b49391b7997f8822d5633250b0b82
Sarah Strange <sarah.strange@availity.com>
2018-03-28 11:09:55 -0400

    conversion values set

--- assets/calculator.js
+++ assets/calculator.js
@@ -6,9 +6,14 @@
 
 		$scope.lumen_options = [375, 600, 900, 1125, 1600];
 		$scope.current_lumens = 600;
-		$scope.current_cost = 13;
-		$scope.current_hours = 4;
+		$scope.current_cost = 12;
+		$scope.current_hours = 3;
 		$scope.total_days = 365;
+
+		$scope.inc_conversion = .0625;
+		$scope.hal_conversion = .0450;
+		$scope.cfl_conversion = .0146;
+		$scope.led_conversion = .0125;
 
 
 	}]);
