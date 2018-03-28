6b84c2ddbf1b665d18e5501f8c609d7513d0ba9e
Sarah Strange <sarah.strange@availity.com>
2018-03-28 11:31:26 -0400

    cost per hour conversion added

--- assets/calculator.js
+++ assets/calculator.js
@@ -16,16 +16,27 @@
 		$scope.led_conversion = .0125;
 
 		$scope.calculate = function(){
-			
 			$scope.inc_wattage = ($scope.current_lumens * $scope.inc_conversion).toFixed(1);
 			$scope.hal_wattage = ($scope.current_lumens * $scope.hal_conversion).toFixed(1);
 			$scope.cfl_wattage = ($scope.current_lumens * $scope.cfl_conversion).toFixed(1);
 			$scope.led_wattage = ($scope.current_lumens * $scope.led_conversion).toFixed(1);
 
+			if($scope.current_hours > 24) {
+				$scope.current_hours =24;
+			}
+
+			var total_hours = $scope.total_days * $scope.current_hours;
+			var cost = $scope.current_cost / 100;
+
+			$scope.inc_cost = ((($scope.inc_wattage * total_hours) / 1000) * cost).toFixed(2);
+			$scope.hal_cost = ((($scope.hal_wattage * total_hours) / 1000) * cost).toFixed(2);
+			$scope.cfl_cost = ((($scope.cfl_wattage * total_hours) / 1000) * cost).toFixed(2);
+			$scope.led_cost = ((($scope.led_wattage * total_hours) / 1000) * cost).toFixed(2);
 		}
 
 		$scope.calculate();
+
 	}]);
-}) ();
+})();
 
 
