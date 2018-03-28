38dff284114a4348531628f5e1465540495c2804
Sarah Strange <sarah.strange@availity.com>
2018-03-28 11:01:59 -0400

    controller and module defined

--- assets/calculator.js
+++ assets/calculator.js
@@ -1,3 +1,20 @@
+(function() {
 
+	var app = angular.module('myCalculator', []);
+
+	app.controller('CalculatorController', ['$scope', function($scope){
+
+		$scope.lumen_options = [375, 600, 900, 1125, 1600];
+		$scope.current_lumens = 600;
+
+	}]);
+
+
+
+
+
+
+
+}) ();
 
 
