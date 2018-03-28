89e11f9f38b151395252acebaa54f4cad928d3f6
Sarah Strange <sarah.strange@availity.com>
2018-03-28 11:06:14 -0400

    defining scope

--- assets/calculator.js
+++ assets/calculator.js
@@ -6,6 +6,10 @@
 
 		$scope.lumen_options = [375, 600, 900, 1125, 1600];
 		$scope.current_lumens = 600;
+		$scope.current_cost = 13;
+		$scope.current_hours = 4;
+		$scope.total_days = 365;
+
 
 	}]);
 
